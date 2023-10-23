d87ca9d97322a4ac5ac2e86fb27bb65c6965bccf
atul3007 <atuldwivedi859@gmail.com>
2023-10-23 12:49:21 +0530

    new

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -6,8 +6,15 @@ const createProduct = async (req, res) => {
   try {
     const { name, slug, description, price, category, shipping } = req.fields;
     const { photo } = req.files;
-    if (!name || !description || !price || !category || !shipping || !photo || photo.size > 1000000)
-     {
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !shipping ||
+      !photo ||
+      photo.size > 1000000
+    ) {
       return res.status(400).send({
         message: "all fields required and pic size should be less than 1mb",
       });
@@ -15,15 +22,15 @@ const createProduct = async (req, res) => {
     const product = new productModel({ ...req.fields, slug: slugify(name) });
     if (photo) {
       product.photo.data = fs.readFileSync(photo.path);
-      product.contentType=photo.type
+      product.contentType = photo.type;
     }
     await product.save();
-    
+
     res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        product,
-      });
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
   } catch (error) {
     res.status(400).send({
       success: false,
@@ -33,6 +40,24 @@ const createProduct = async (req, res) => {
   }
 };
 
+const getProduct = async (req, res) => {
+  try {
+    const product = await productModel.find({}).select("-photo").limit(10).sort({createdAt:-1});
+    res.status(200).send({
+        success: true,
+        message: product,
+        total_count : product.length
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+      message: "Error in getting product",
+    });
+  }
+};
+
 module.exports = {
   createProduct,
+  getProduct,
 };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,12 @@
 const express=require("express");
-const { createProduct } = require("../controller/productController");
+const { createProduct, getProduct } = require("../controller/productController");
 const { requireSignin, checkRole } = require("../middlewares/atuhMiddleware");
 const productRouter=express.Router();
 const formidableMiddleware = require('express-formidable');
 
 productRouter.post("/create-product",requireSignin,checkRole,formidableMiddleware(),createProduct)
+
+productRouter.get("/get-product",getProduct)
 
 module.exports={
     productRouter
