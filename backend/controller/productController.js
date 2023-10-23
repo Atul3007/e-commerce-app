956949eb52d0c6d013c0f02e6b56aa757bfe3a13
atul3007 <atuldwivedi859@gmail.com>
2023-10-23 12:28:36 +0530

    new

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,11 +1,38 @@
-const createProduct=async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const { productModel } = require("../models/productModel");
+const { default: slugify } = require("slugify");
+const fs = require("fs");
 
-module.exports={
-    createProduct
-}
+const createProduct = async (req, res) => {
+  try {
+    const { name, slug, description, price, category, shipping } = req.fields;
+    const { photo } = req.files;
+    if (!name || !description || !price || !category || !shipping || !photo || photo.size > 1000000)
+     {
+      return res.status(400).send({
+        message: "all fields required and pic size should be less than 1mb",
+      });
+    }
+    const product = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.contentType=photo.type
+    }
+    await product.save();
+    
+    res.status(201).json({
+        success: true,
+        message: "Product created successfully",
+        product,
+      });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+      message: "Error in creating product",
+    });
+  }
+};
+
+module.exports = {
+  createProduct,
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,8 +2,9 @@ const express=require("express");
 const { createProduct } = require("../controller/productController");
 const { requireSignin, checkRole } = require("../middlewares/atuhMiddleware");
 const productRouter=express.Router();
+const formidableMiddleware = require('express-formidable');
 
-productRouter.post("/create-product",requireSignin,checkRole,createProduct)
+productRouter.post("/create-product",requireSignin,checkRole,formidableMiddleware(),createProduct)
 
 module.exports={
     productRouter
