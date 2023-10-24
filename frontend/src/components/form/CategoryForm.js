20b74d710c2b07f03628f34d351eb01903fd07a8
atul3007 <atuldwivedi859@gmail.com>
2023-10-24 18:11:49 +0530

    new

--- frontend/src/components/form/CategoryForm.js
+++ frontend/src/components/form/CategoryForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const CategoryForm = () => {
+  const [value, setValue] = useEffect("");
   return (
     <>
-    
-    </>
-  )
-}
+      <form>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="enter new category"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
 
-export default CategoryForm
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CategoryForm;
