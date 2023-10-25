b04856d6c39fa8ebd2467cbdfc847f4b667ced69
atul3007 <atuldwivedi859@gmail.com>
2023-10-25 20:08:20 +0530

    new

--- frontend/src/pages/admin/CreateProduct.js
+++ frontend/src/pages/admin/CreateProduct.js
@@ -1,23 +1,104 @@
-import React from 'react'
-import Layout from './../../components/layouts/Layout';
-import AdminMenu from '../../components/layouts/AdminMenu';
+import React, { useEffect, useState } from "react";
+import Layout from "./../../components/layouts/Layout";
+import AdminMenu from "../../components/layouts/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Select } from "antd";
+const { Option } = Select;
 
 const CreateProduct = () => {
+  const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [shipping, setShipping] = useState("");
+
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8000/api/category/all"
+      );
+      setCategories(data?.data);
+    } catch (error) {
+      console.log(error);
+      toast.error("error in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
-      <Layout>
-      <div className='container-field'>
-        <div className='row'>
-          <div className='col-md-3'>
-             <AdminMenu/>
+    <Layout>
+      <div className="container-field">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
           </div>
-          <div className='col-md-9'>
-            <div className="card w-75 " style={{marginTop:'4%', padding:"20px"}}>
+          <div className="col-md-9">
+            <div
+              className="card w-75 "
+              style={{ marginTop: "4%", padding: "20px" }}
+            >
               <h4> CreateProducts</h4>
+              <div className="m-1 w-75">
+                <Select
+                  bordered={false}
+                  placeholder="select a category"
+                  size={"large"}
+                  showSearch
+                  className="form-select mb-3"
+                  onChange={(value) => {
+                    setCategory(value);
+                  }}
+                >
+                  {categories?.map((c) => (
+                    <Option key={c.id} value={c.name}>
+                      {c.name}
+                    </Option>
+                  ))}
+                </Select>
+                <label className="btn btn-outline-secondary col-md-12">
+                  {photo ? photo.name : "Upload photo"}
+                  <input
+                    type="file"
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => {
+                      setPhoto(e.target.files[0]);
+                    }}
+                    hidden
+                  />
+                </label>
+              </div>
+              <div className="mb-3">
+                {photo && (
+                  <div className="text-center">
+                    <img
+                      src={URL.createObjectURL(photo)}
+                      alt="product photo"
+                      height={"200px"}
+                      className="img img-responsive"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="Enter product name"
+                  className="form-control"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div></Layout>
-  )
-}
+      </div>
+    </Layout>
+  );
+};
 
-export default CreateProduct
+export default CreateProduct;
