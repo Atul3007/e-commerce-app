61300f62f0159e06ec3efcf6d73fe7168f92507e
atul3007 <atuldwivedi859@gmail.com>
2023-10-31 12:50:37 +0530

    new

--- frontend/src/components/form/Search.js
+++ frontend/src/components/form/Search.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { useSearch } from "../../context/Search";
-const Search = () => {
+import axios from "axios";
+const Search =async () => {
     const [search,setSearch]=useSearch();
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        try {
+            const {product}=await axios.get(`http://localhost:8000/api/product//search-product/${search}`)
+            setValues({...search,result:product})
+        } catch (error) {
+            console.log({error})
+        }
+    }
   return (
     <div>
-          <form className="d-flex" role="search">
+          <form className="d-flex" role="search" onSubmit={handleSubmit}>
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
+              value={search.keywords}
+              onChange={(e)=>setSearch({...values,keyword:e.target.value})}
             />
             <button className="btn btn-outline-success" type="submit">
               Search
