eb48908d5116af535f9defbb36a25df4d47c585b
atul3007 <atuldwivedi859@gmail.com>
2023-11-01 11:24:23 +0530

    new

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -4,12 +4,30 @@ import { useSearch } from '../context/Search'
 
 const Search = () => {
     const [search,setSearch]=useSearch();
+    const result=search.result;
+    console.log(result)
   return (
     <Layout>
       <div className="container">
+        <div className="card-grid">
         <div className="text-center">
             <h1>Search Results</h1>
-             {console.log(search)}
+             {result.map((p)=>(
+                 <div className="card" style={{ width: "18rem" }} >
+                 <img
+                   src={`http://localhost:8000/api/product/product-photo/${p._id}`}
+                   className="card-img-top"
+                   style={{width:"200px",height:"200px",marginLeft:"50px",marginTop:"20px"}}
+                   alt="product_photo"
+                 />
+                 <div className="card-body" style={{marginLeft:"50px"}}>
+                   <h5 className="card-title">Title : {p.name}</h5>
+                   <h6 className="card-text">Description : {p.description}</h6>
+                   <p className="card-text">Price : {p.price}</p>
+                 </div>
+               </div>
+             ))}
+        </div>
         </div>
       </div>
     </Layout>
